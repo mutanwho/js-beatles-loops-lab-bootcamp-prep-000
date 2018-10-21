1d169024b8c564e217101f22e1bfe4dab93ab797
Michelle M <mutanwho@gmail.com>
2018-10-21 17:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ function johnLennonFacts(){
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
     ];
+    var array = [];
+    var i = 0;
+    while (array.length <facts.length) {
 }
