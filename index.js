791176870a5c21d27c5a6111a0e89a03284ca269
Michelle M <mutanwho@gmail.com>
2018-10-21 17:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay(["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"], ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"])
+ console.log(theBeatlesPlay(["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"], ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]))
+ function johnLennonFacts(facts) {
+  var i = 0
+  while(i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+        i++
+    }
+    return facts
+ }
